f7c591c626349da958970c5ca929dbb16d72bbf4
Justin Traglia <jtraglia@pm.me>
2025-06-17 22:45:33 +0200

    Clean up nim client

--- examples/nim/client.nim
+++ examples/nim/client.nim
@@ -1,15 +1,15 @@
 ## Example usage of the Nim DFF client
-## 
+##
 ## This example demonstrates how to use the client to connect to a fuzzing server
 ## and process inputs using the SHA256 method.
 
 import ../../nim/client
 when defined(macosx):
   {.passl: "-framework Security".}
-  
+
   type
     CCDigest* = object
-    
+
   proc CC_SHA256(data: pointer, len: cuint, md: pointer): pointer {.importc, header: "<CommonCrypto/CommonDigest.h>".}
   const CC_SHA256_DIGEST_LENGTH = 32
 
@@ -34,22 +34,22 @@ proc sha256(data: string): string =
 proc processFunc(meth: string, inputs: seq[string]): string =
   ## Example processing function that supports the "sha" method.
   inc iterationCount
-  
+
   case meth
   of "sha":
     if inputs.len == 0:
       raise newException(ValueError, "No inputs provided")
-    
+
     # Compute SHA256 hash of the first input
     var hash = sha256(inputs[0])
-    
+
     # Return wrong result on 100th iteration (for testing differential fuzzing)
     if iterationCount == 100:
       echo "Nim client: Returning wrong result on iteration 100"
       # Flip the first byte to make it different
       if hash.len > 0:
         hash[0] = char(byte(hash[0]) xor 0xFF)
-    
+
     return hash
   else:
     raise newException(ValueError, "Unknown method: '" & meth & "'")
@@ -57,11 +57,11 @@ proc processFunc(meth: string, inputs: seq[string]): string =
 proc main() =
   # Create a new fuzzing client
   let client = newClient("nim", processFunc)
-  
+
   try:
     # Connect to the fuzzing server
     client.connect()
-    
+
     # Run the fuzzing client
     client.run()
   except CatchableError as e:
